819237081092aca26eb138a0f0cf4964708efa7e
idohamrany <idohamrany13882002@gmail.com>
2024-10-02 17:09:30 +0300

    First upload

---
+++ exc1.py
@@ -0,0 +1,35 @@
+import random
+
+# exc a
+l1: list[int] = [x for x in range(95, 106)]
+print(f"l1: {l1}")  # to check
+
+# exc b
+l2: list[int] = [x for x in range(10, 21, 2)]
+print(f"l2: {l2}")  # to check
+
+# exc c
+l3: list[bool] = [random.choice([True, False]) for _ in range(5)]
+print(f"l3: {l3}")
+
+# exc d
+l4: list[int] = [random.randint(1, 101) for _ in range(10)]
+print(f"l4: {l4}")  # to check
+
+# exc e
+l5: list[int] = [x for x in l4 if x > 50]
+print(f"l5: {l5}")  # to check
+
+# exc f
+# you can make it in one row, change the range to 50 to 100
+
+# exc g
+sen: str = input("enter a string: ")
+l7: list[str] = [x for x in sen if x != "t" and x != " "]
+print(f"l7: {l7}")
+
+# exc h
+l8: list[int] = [random.randint(10, 100) for _ in range(10)]
+print(f"l8: {l8}")
+l9: list[int] = [x // 10 for x in l8]
+print(f"l9: {l9}")
---
+++ exc2.py
@@ -0,0 +1,36 @@
+import random
+
+random.random
+
+# exc a
+# try - except,  is trying a code that is problematic and catching the error and handling it so the code won't crash.
+
+# exc b
+# if you catch the error before it occurs, you can handle it therefore the code will not crash.
+
+# exc c
+try:
+    x: int = 88 / 0
+    print(x)
+except:
+    print(" you can't divide 88 by 0")
+
+# exc d
+age: int = int(input("enter your age: "))
+try:
+    if age < 18:
+        raise AttributeError(f" you are {age} and that is too young")
+except:
+    print(f"{age} is too young")
+
+# exc e
+l1: list[int] = [random.randint(1, 100) for _ in range(4)]
+print(f"l1: {l1}")  # to check
+while True:
+    num: int = int(input("enter a number: "))
+    if num == -999:
+        break
+    try:
+        print (l1[num])
+    except:
+        print (f" {num} is not an index in the list")
